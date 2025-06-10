decdc5b2b11b64f65325c79e3e4c524fffacd97a
akshxdevs <akshxdevs@gmail.com>
2025-06-10 12:48:09 +0530

    v1.1

--- app/Components/WalletGenerator.tsx
+++ app/Components/WalletGenerator.tsx
@@ -7,15 +7,17 @@ import { useEffect, useState } from "react";
 
 export const WalletGenerator = () => {
     const theme = useRecoilValue(themeState);
-    const [mnemonic,setMnemonic] = useState("");
-    const [walletSelected,setWalletSelected] = useState(false)
-    const [WalletName,setWalletName] = useState("")
+    const [mnemonic,setMnemonic] = useState<string>("");
+    const [WalletName,setWalletName] = useState<string>("")
     const [showMn,setShowMn] = useState(false);
     const [showDownArrow,setShowDownArrow] = useState(false);
+
     useEffect(()=>{
+        const savedWalletName = localStorage.getItem("walletName");
+        if (savedWalletName) setWalletName(savedWalletName)
         const savedMn = localStorage.getItem("mnemonics");
-        if (savedMn) setMnemonic(savedMn);
-    },[])
+        if (savedMn) setMnemonic(savedMn)
+    },[]);
     const handleMnemonic = async() => {
         if (!mnemonic) {
             const mn = await generateMnemonic();
@@ -25,7 +27,7 @@ export const WalletGenerator = () => {
     }
 
     return <div>
-        {walletSelected ? (
+        {WalletName ? (
             <div className="max-w-screen-lg mx-auto">
                 <div className={`flex justify-between border ${theme === "light" ? "border-gray-900 " :"border-gray-400 "} p-10 rounded-lg`}>
                     <h1 className="text-2xl font-semibold">Your Seed Phrase</h1>
@@ -42,7 +44,7 @@ export const WalletGenerator = () => {
                     <button onClick={()=>{
                         setShowMn(true);
                         setShowDownArrow(true);
-                        handleMnemonic}}>
+                        handleMnemonic()}}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                             </svg>
@@ -65,7 +67,7 @@ export const WalletGenerator = () => {
                         <button className={`px-8 py-2 mb-2 ${theme === "light" ? "bg-black text-white" : "bg-gray-100 text-black" } rounded-lg`}>Add Wallet</button>
                         <button className={`px-8 py-2 mb-2 ${theme === "light" ? "bg-black text-white" : "bg-gray-100 text-black" } rounded-lg`} onClick={()=>{
                             localStorage.clear();
-                            setWalletSelected(false);
+                            setWalletName("");
                         }}>Clear Wallets</button>
                     </div>
                 </div>
@@ -76,11 +78,13 @@ export const WalletGenerator = () => {
                 <p className={`text-md ${theme === "light" ? "text-gray-900" : "text-gray-100" } pb-3`}>choose a blockchain to get started.</p>
                 <div className="flex gap-2">
                     <button className={`px-8 py-2 mb-2 ${theme === "light" ? "bg-black text-white" : "bg-gray-100 text-black" } rounded-lg`} onClick={()=>{
-                            setWalletName("Solana")
-                            setWalletSelected(true);}}>Solana</button>
+                        const name = "Solana"
+                        setWalletName(name)
+                        localStorage.setItem("walletName",name);}}>Solana</button>
                     <button className={`px-8 py-2 mb-2 ${theme === "light" ? "bg-black text-white" : "bg-gray-100 text-black" } rounded-lg`} onClick={()=>{
-                        setWalletName("Ethereum")
-                        setWalletSelected(true)}}>Ethereum</button>
+                        const name = "Ethereum"
+                        setWalletName(name)
+                        localStorage.setItem("walletName",name);}}>Ethereum</button>
                 </div>
             </div>
         )}
